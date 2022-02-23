ec8e1fb065b7e8206558929908d2219b34610e2b
Mayank Arora <mayankarora.email@gmail.com>
2022-02-23 09:44:47 +0530

    Time: 4 ms (69.57%), Space: 7.7 MB (57.06%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> ourResult;
+        vector<int> singleIter = nums;
+		ourResult.push_back(singleIter);
+        next_permutation(singleIter.begin(), singleIter.end());
+        while (singleIter != nums)
+        {
+            ourResult.push_back(singleIter);
+            next_permutation(singleIter.begin(), singleIter.end());
+        }
+        return ourResult;
+    }
+};
