a98ba532f0ec4a10424c906b7dde47416727cc77
Mayank Arora <mayankarora.email@gmail.com>
2022-08-17 07:40:42 +0530

    Time: 3 ms (89.02%), Space: 7.7 MB (71.29%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    int KMPtheorm(string s) {
-        int LPS[s.size()];
-        LPS[0] = 0;
-        int i = 1;
-        int len = 0;
-        while (i < s.size()) {
-            if (s[i] == s[len]) {
-                len++;
-                LPS[i++] = len;
-            } else {
-                if (len > 0) {
-                    len = LPS[len - 1];
-                } else {
-                    LPS[i] = 0;
-                    i++;
-                }
+string shortestPalindrome(string s)
+{
+    int n = s.size();
+    string rev(s);
+    string str(s);
+    reverse(rev.begin(),rev.end());
+    s=s+"#"+rev;
+    int lps[s.size()];
+    lps[0] = 0;
+    int i = 1, j = 0;
+    while (i < s.size())
+    {
+        if (s[j] == s[i])
+        {
+            lps[i] = j + 1;
+            i++;
+            j++;
+        }
+        else
+        {
+            if (j != 0)
+            {
+                j = lps[j - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
             }
         }
-        return LPS[s.size() - 1];
     }
-    string shortestPalindrome(string s) {
-        string part1 = s;
-        reverse(s.begin(), s.end());
-        string part2 = s;
-        string str = part1 + "#" + part2;
-        
-        int len = KMPtheorm(str);
-        
-        reverse(s.begin(), s.end());
-        string ans = s.substr(len);
-        
-        reverse(ans.begin(), ans.end());
-        return ans + s;
-    }
+
+    int k=lps[s.size()-1];
+    return rev.substr(0,n-k)+str;
+    
+}
+    
+    
 };
