26fa5c1807d20d224468f826a94e3760d3fa8c00
Mayank Arora <mayankarora.email@gmail.com>
2022-08-21 09:23:45 +0530

    Time: 763 ms (71.46%), Space: 13.1 MB (51.54%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,73 +1,64 @@
 class Solution {
 public:
-    
-    vector<string> ans;
-    int dx[4]={0,0,1,-1};
-    int dy[4]={1,-1,0,0};
-    
-    class Trie {
+    class Node{
         public:
-            Trie *arr[26];
-            bool isEnd = false;
+        Node* links[26]={NULL};
+        string flag;
+        void put(char ch,Node* node){
+            links[ch-'a']=node;
+        }
+        Node* next(char ch){
+            return links[ch-'a'];
+        }
+        void set(string w){
+            flag=w;
+        }
+        int isEnd(){
+            return flag.length();
+        }
     };
+    int dr[4] = {1, -1, 0, 0}; 
+    int dc[4] = {0, 0, -1, 1}; 
+    int n,m;
+    void dfs(int r, int c, vector<vector<char>>& grid, Node *trie, vector<string>&ans) {
+        if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c]=='0' || trie->next(grid[r][c])==nullptr) return;
+        char ch=grid[r][c];
+        trie=trie->next(ch);
+        if(trie->isEnd()){
+            ans.push_back(trie->flag);
+            trie->flag="";
+        }
+        grid[r][c] = '0';
+        for (int i = 0; i < 4; i++) { 
+            dfs(r + dr[i], c + dc[i],grid,trie,ans);
+        }
+        grid[r][c]=ch;
+    }
     
-    void insert(string &word, Trie* root){
-        Trie *cur = root;
-        for(int i=0; i<word.size(); ++i){
-            int idx = word[i]-'a';
-            if(!cur->arr[idx]){
-                cur->arr[idx] = new Trie();
+    void insert(string s,Node *root){
+        Node *node=root;
+        for(int i=0;i<s.length();i++){
+            if(node->links[s[i]-'a']==nullptr){
+                node->put(s[i],new Node);
             }
-            cur=cur->arr[idx];
+            node=node->next(s[i]);
         }
-        cur->isEnd = true;
-        return;
-    }
-    
-    bool isValid(vector<vector<char>>& board, int x, int y){
-        int row = board.size(), col = board[0].size();
-        if(x<0 || x>=row || y<0 || y>=col || board[x][y]=='*') return false;
-        return true;
-    }
-    
-    void search(vector<vector<char>>& board, int x, int y, Trie* root, string& str){
-        if(board[x][y]=='*') return;
-        
-        int idx = board[x][y]-'a';
-        if(!root->arr[idx]) return;
-        
-        str.push_back(board[x][y]);
-        
-        char ch = board[x][y];
-        board[x][y]='*';
-        
-        if(root->arr[idx]->isEnd){
-            ans.push_back(str);
-            root->arr[idx]->isEnd = false;
-        }
-        
-        for(int i=0;i<4;++i){
-            int x1 = x+dx[i], y1 = y+dy[i];
-            if(isValid(board,x1,y1)) search(board,x1,y1,root->arr[idx],str);
-        }
-        
-        board[x][y]=ch;
-        str.pop_back();
-        return;
+        node->set(s);
     }
     
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie *root = new Trie();
-        for(auto &word : words) insert(word,root);
-        
-        int row = board.size(), col = board[0].size();
-        string str="";
-        for(int i=0;i<row;++i){
-            for(int j=0;j<col;++j){
-                search(board,i,j,root,str);
-            }
+        n=board.size();
+        m=board[0].size();
+        Node *trie=new Node();
+        for(auto w:words){
+            insert(w,trie);
         }
-        
+        vector<string>ans;
+        for(int j=0;j<n;j++){
+            for(int k=0;k<m;k++){
+                    dfs(j,k,board,trie,ans);
+                }
+            }
         return ans;
     }
 };
