b3f8cd1d84853b11379cda1aa1439612a7649649
Mayank Arora <mayankarora.email@gmail.com>
2022-08-23 08:02:27 +0530

    Time: 4 ms (22.29%), Space: 5.7 MB (97.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climbStairs(int n) {
         int t[n+1];
-        for(int i=0;i<=n;i++) t[i]=0;
+        for(int i=0;i<=n-2;i++) t[i]=0;
         t[1]=1;
         if(n>=2) t[2]=2;
         for(int i=3;i<=n;i++){
